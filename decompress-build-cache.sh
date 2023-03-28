0e45ada386b0d1eaacdefabb53147cdb1bfc3b38
anakinxc <103552181+anakinxc@users.noreply.github.com>
2023-03-28 13:58:03 +0800

    Move common ci scripts to devtools (#21)

---
+++ decompress-build-cache.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+#
+# Copyright 2023 Ant Group Co., Ltd.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#   http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+FILENAME=$1
+CACHE_FILE=~/.cache/$FILENAME.tar.gz
+
+if test -f "$CACHE_FILE"; then
+    echo "$CACHE_FILE exists."
+    cd ~/.cache
+    tar -xzf $FILENAME.tar.gz
+    rm -f $FILENAME.tar.gz
+else
+    echo "$CACHE_FILE does not exist"
+fi
